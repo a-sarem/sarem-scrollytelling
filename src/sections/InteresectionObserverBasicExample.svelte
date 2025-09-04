2bc4c34a86f2352a3be18f1fba038696e15d8c2e
a-sarem <sagha5@gmu.edu>
2025-09-04 11:05:14 -0400

    conclusion page

--- src/sections/InteresectionObserverBasicExample.svelte
+++ src/sections/InteresectionObserverBasicExample.svelte
@@ -24,24 +24,24 @@
     {/snippet}
 
     {#snippet scrolly()}
-      <ArticleText>
-        Looking statewide, North Carolina has over 9,000 Black-owned employer
-        firms, with a Black population of 20.6%. Which shows strong
-        representation in entrepreneurship.
-      </ArticleText>
+        <ArticleText>
+          Looking statewide, North Carolina has over 9,000 Black-owned employer
+          firms, with a Black population of 20.6%. Which shows strong
+          representation in entrepreneurship.
+        </ArticleText>
 
-      <ArticleText>
-        Even in a state with high Black business ownership, unequal outcomes
-        still exist, which suggests that access to capital (not just the number
-        of firms) is a major factor
-      </ArticleText>
+        <ArticleText>
+          Even in a state with high Black business ownership, unequal outcomes
+          still exist, which suggests that access to capital (not just the
+          number of firms) is a major factor
+        </ArticleText>
 
-      <ArticleText>
-        But if we go back to my original question, do these businesses achieve
-        the same outcomes as white-owned firms? How much of that gap is
-        explained by unequal access to capital, such as disparities in loan
-        approvals or investment opportunities?
-      </ArticleText>
+        <ArticleText>
+          But if we go back to my original question, do these businesses achieve
+          the same outcomes as white-owned firms? How much of that gap is
+          explained by unequal access to capital, such as disparities in loan
+          approvals or investment opportunities?
+        </ArticleText>
     {/snippet}
   </Scroller>
 </div>
@@ -53,16 +53,6 @@
     object-fit: fit;
     border-radius: 10px;
     box-shadow: 0 4px 12px;
-  }
-
-  .list {
-    margin: 0px 20px;
-    background-color: #ff99fc;
-    color: #8427c9;
-    border: solid #8427c9 3px;
-    border-radius: 20px;
-    box-shadow: 16px 16px #8aa6df;
-    width: 100%;
   }
 
   h3 {
